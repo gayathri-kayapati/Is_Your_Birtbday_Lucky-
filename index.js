02198935bfc75de6adeb2539cbe648bf6430b437
gayathri-kayapati <gayathrikayapati@gmail.com>
2023-01-21 17:19:23 +0530

    js:given reference to the input,process & output elements

---
+++ index.js
@@ -0,0 +1,4 @@
+var dateOfBirth = document.querySelector("#date-of-birth");
+var luckyNumber = document.querySelector("#lucky-number");
+var checkBtn = document.querySelector("#check-btn");
+var outputBox = document.querySelector("#output");
