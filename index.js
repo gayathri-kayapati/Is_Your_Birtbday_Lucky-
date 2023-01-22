070263b7c575b9b3f25353e23ae877a2250a9bc0
gayathri-kayapati <gayathrikayapati@gmail.com>
2023-01-22 11:31:11 +0530

    js:added functionality for sum of date of birth

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ var dateOfBirth = document.querySelector("#date-of-birth");
 var luckyNumber = document.querySelector("#lucky-number");
 var checkBtn = document.querySelector("#check-btn");
 var outputBox = document.querySelector("#output");
+
+function sumOfDateOfBirth() {
+  var dobValue = dateOfBirth.value;
+  var dob = dobValue.replaceAll("-", "");
+  var sum = 0;
+  for (let i = 0; i < dob.length; i++) {
+    sum = sum + Number(dob[i]);
+  }
+  return sum;
+}
